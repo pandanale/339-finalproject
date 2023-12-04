19069b584db0b56fd1e595f8427e7c953b002e21
pandanale <amtan2001@gmail.com>
2023-12-04 18:37:30 -0500

    mailer 2

--- script.js
+++ script.js
@@ -8,49 +8,43 @@ document.addEventListener("DOMContentLoaded", function() {
     const promoBanner = document.querySelector('.promo-banner');
     promoBanner.innerHTML = `<span class="marquee">${promoMessages.join(' ')}</span>`;
 });
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    if(window.location.pathname.includes('recipes.html')) {
-      setTimeout(function() {
-          document.body.innerHTML += `
-          <div class="popup-overlay">
-              <div class="popup-content">
-              <h2>Join the OliaRosa Mailing List!</h2>
-              <p>Sign up to receive more recipes, hear about our farm and receive exclusive offers!</p>
-              <form id="emailSignup">
-                  <input type="email" id="email" name="email" placeholder="Enter your email address" required>
-                  <button type="submit">Sign Up</button>
-              </form>
-              <button class="close-popup">X Close</button>
-              </div>
-          </div>
-          `;
-          document.querySelector('.close-popup').addEventListener('click', function() {
-          document.querySelector('.popup-overlay').style.display = 'none';
-          });
-          document.getElementById('emailSignup').addEventListener('submit', function(e) {
-          e.preventDefault();
-          // Here, you would normally handle the form submission, like sending data to your server
-          alert('Thank you for signing up! We cant wait to share more about OliaRosa with you!');
-          document.querySelector('.popup-overlay').style.display = 'none';
-          });
-      }, 10000); // 10 seconds
-    }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('emailSubscription').addEventListener('click', function() {
-      document.getElementById('popupOverlay').style.display = 'flex';
-    });
-  
+  setTimeout(function() {
+    // Create the overlay
+    const overlay = document.createElement('div');
+    overlay.className = 'popup-overlay';
+    overlay.style.display = 'flex';
+
+    // Create the popup content
+    const popupContent = document.createElement('div');
+    popupContent.className = 'popup-content';
+    popupContent.innerHTML = `
+      <h2>Join the OliaRosa Mailing List!</h2>
+      <p>Sign up to receive more recipes, hear about our farm and receive exclusive offers!</p>
+      <form id="emailSignup">
+        <input type="email" id="email" name="email" placeholder="Enter your email address" required>
+        <button type="submit">Sign Up</button>
+      </form>
+      <button class="close-popup">X Close</button>
+    `;
+
+    // Append popup content to overlay
+    overlay.appendChild(popupContent);
+
+    // Append overlay to body
+    document.body.appendChild(overlay);
+
+    // Close popup logic
     document.querySelector('.close-popup').addEventListener('click', function() {
-      document.getElementById('popupOverlay').style.display = 'none';
+      overlay.style.display = 'none';
     });
-  
+
+    // Form submission logic
     document.getElementById('emailSignup').addEventListener('submit', function(e) {
       e.preventDefault();
-      alert('Thank you for signing up!');
-      document.getElementById('popupOverlay').style.display = 'none';
+      alert('Thank you for signing up! We cant wait to share more about OliaRosa with you!');
+      overlay.style.display = 'none';
     });
-  });
+
+  }, 10000); // 10 seconds
+});
